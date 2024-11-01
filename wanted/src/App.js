5fea3eed0da6069736a7aee83a19a9fceacec2f6
gergokiss04 <94444225+gergokiss04@users.noreply.github.com>
2024-11-01 14:10:01 +0100

    Header módosítása

--- wanted/src/App.js
+++ wanted/src/App.js
@@ -1,136 +1,31 @@
-import './App.css';
-import { NavLink, Routes, Route } from "react-router-dom";
-import Home from './Home';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+function Home() { return <h2>Főoldal</h2>; }
+function Electronics() { return <h2>Műszaki cikkek</h2>; }
+function Books() { return <h2>Könyvek</h2>; }
+function Games() { return <h2>Társasjátékok</h2>; }
+function Clothing() { return <h2>Ruhák</h2>; }
+function PostAd() { return <h2>Hirdetés feladása</h2>; }
+function Login() { return <h2>Bejelentkezés/Regisztráció</h2>; }
 
 function App() {
   return (
-    <div className="App">
-      {/*NAVBAR KEZDETE*/}
-      <nav className="navbar sticky-top navbar-expand-lg navbar-dark fw-bold shadow-5-strong header-footer-background-image">
-        <div className="container-fluid">
-          <NavLink
-            className="navbar-brand"
-            title="brandlogo"
-            style={{ textDecoration: "none" }}
-            to="/"
-            onClick={() => window.scrollTo(0, 0)}
-          >
-            <img
-              src="/img/logo.webp"
-              alt=""
-              width="200"
-              height="40"
-              className="d-inline-block align-text-top"
-            />
-          </NavLink>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <NavLink id="fooldal" style={{ textDecoration: "none" }} to="/">
-                <li className="nav-item nav-link">Főoldal</li>
-              </NavLink>
-              <NavLink
-                id="konyvajanlo"
-                style={{ textDecoration: "none" }}
-                to="/bookrecommendation"
-              >
-                <li className="nav-item nav-link">Termékek</li>
-              </NavLink>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      {/*NAVBAR VÉGE*/}
-
-      {/* Útvonalak */}
+    <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-
+        <Route path="/products/electronics" element={<Electronics />} />
+        <Route path="/products/books" element={<Books />} />
+        <Route path="/products/games" element={<Games />} />
+        <Route path="/products/clothing" element={<Clothing />} />
+        <Route path="/post-ad" element={<PostAd />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
-
-       {/*LÁBLÉC KEZDETE*/}
-       <footer className="text-center text-lg-start bg-dark text-muted header-footer-background-image">
-        <section>
-          <div className="container text-center text-md-start ">
-            <div className="row ">
-              <div className=" col-auto col-md-auto col-lg-4 col-xl-3 mx-auto mb-4">
-                <br></br>
-                <h6 className="text-uppercase fw-bold mb-4">
-                  <i className="bold-white-color">Wanted</i>
-                </h6>
-                <p className="bold-white-color">
-                  Szeretettel köszöntjük a Wanted honlapján!
-                </p>
-                <img
-                  title="bblogo"
-                  decoding="async"
-                  src="/img/footerlogo.webp"
-                  alt="brand"
-                  width="240"
-                  height="120"
-                />
-              </div>
-              <div className="col-auto col-md-auto col-lg-4 col-xl-3 mx-auto mb-4">
-                <br></br>
-                <iframe
-                  title="myFrame"
-                  decoding="async"
-                  src=""
-                  width="300"
-                  height="200"
-                  style={{ border: "0" }}
-                  allowFullScreen=""
-                  loading="lazy"
-                  referrerPolicy="no-referrer-when-downgrade"
-                ></iframe>
-              </div>
-
-              <div className="col-auto col-md-auto col-lg-4 col-xl-4 mx-auto mb-md-0 mb-4">
-                <br></br>
-                <h6 className="text-uppercase fw-bold mb-4 bold-white-color">
-                  Elérhetőség:
-                </h6>
-                <ul>
-                  <li className="bold-white-color">Telefon: +36 06 403 330</li>
-                  <li className="bold-white-color footermargin">
-                    E-mail:wantedinfo@gmail.com
-                  </li>
-                  <li>
-                    <a
-                      target="_blank"
-                      rel="noreferrer"
-                      className="bold-white-color none-text-decoration"
-                      href="https://uni-eszterhazy.hu/"
-                    >
-                      <img
-                        src="/img/logo.webp"
-                        decoding="async"
-                        title="logo"
-                        alt="brand"
-                        width="80"
-                        height="80"
-                      />
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </section>
-      </footer>
-      {/*Lábléc vége*/}
-    </div>
+    </Router>
   );
 }
 
---
+++ wanted/src/Header.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Header() {
+  return (
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          {/* Bal oldalon a Termékek és Hirdetés feladása */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav me-auto">
+              {/* Termékek lenyíló menü */}
+              <li className="nav-item dropdown">
+                <NavLink
+                  className="nav-link dropdown-toggle"
+                  to="#"
+                  id="productsDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Termékek
+                </NavLink>
+                <ul className="dropdown-menu" aria-labelledby="productsDropdown">
+                  <li><NavLink className="dropdown-item" to="/products/electronics">Műszaki cikkek</NavLink></li>
+                  <li><NavLink className="dropdown-item" to="/products/books">Könyvek</NavLink></li>
+                  <li><NavLink className="dropdown-item" to="/products/games">Társasjátékok</NavLink></li>
+                  <li><NavLink className="dropdown-item" to="/products/clothing">Ruhák</NavLink></li>
+                </ul>
+              </li>
+              
+              {/* Hirdetés feladása */}
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/post-ad">Hirdetés feladása</NavLink>
+              </li>
+            </ul>
+
+            {/* Középen a logó */}
+            <NavLink className="navbar-brand mx-auto" to="/">
+              <img
+                src="/img/logo.webp"
+                alt="Logó"
+                width="120"
+                height="40"
+                className="d-inline-block align-top"
+              />
+            </NavLink>
+
+            {/* Jobb oldalon a Bejelentkezés/Regisztráció */}
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/login">Bejelentkezés/Regisztráció</NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
