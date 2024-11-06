f3bc790675bbe721f089614a6aafdaaf0bbc5bec
gergokiss04 <94444225+gergokiss04@users.noreply.github.com>
2024-11-06 15:04:58 +0100

    Carousel módosítása

--- wanted/src/Home.js
+++ wanted/src/Home.js
@@ -4,27 +4,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export function Home() {
   return (
     <div>
-     <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="img/carousel1.jpg" class="d-block w-100" alt="..."></img>
-    </div>
-    <div class="carousel-item">
-      <img src="img/carousel2.jpg" class="d-block w-100" alt="..."></img>
-    </div>
-    <div class="carousel-item">
-      <img src="img/carousel3.jpg" class="d-block w-100" alt="..."></img>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
+      {/* Carousel */}
+      <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner" style={{ height: '50vh' }}>
+          <div className="carousel-item active">
+            <img src="img/pr.jpg" className="d-block w-100 h-100 object-fit-cover" alt="Slide 1" />
+          </div>
+          <div className="carousel-item">
+            <img src="img/carousel.jpg" className="d-block w-100 h-100 object-fit-cover" alt="Slide 2" />
+          </div>
+          <div className="carousel-item">
+            <img src="img/carousel2.jpg" className="d-block w-100 h-100 object-fit-cover" alt="Slide 3" />
+          </div>
+        </div>
+        
+        {/* Carousel Controls */}
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
 
       {/* Keresősáv */}
       <div className="p-5 m-auto text-center content bg-lavender img-down">
