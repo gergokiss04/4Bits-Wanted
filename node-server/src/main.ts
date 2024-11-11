3f167e7ecb1cee1da20c16b19e8bde8bf42575b3
Kiki <mullerkrisztianbusiness@gmail.com>
2024-11-11 05:21:36 +0100

    Confing betöltés.

--- node-server/src/main.ts
+++ node-server/src/main.ts
@@ -1,22 +1,29 @@
 // A /wanted/build-be teszi a react a build eredményét.
 
 // Load HTTP module
-import http from "http";
+import http from 'http'
+import * as fs from 'fs'
 
-const hostname = "127.0.0.1";
-const port = 8000;
+import { Config } from './config'
+
+
+const config = new Config(
+                 JSON.parse(
+                 fs.readFileSync(
+                 process.env.WANTED_CONFIG || 'wanted-config.json', 'utf-8')
+))
 
 // Create HTTP server
 const server = http.createServer(function (req, res) {
-  console.log(`Request: ${req.url}`); // HACK
+  console.log(`Request: ${req.url}`) // HACK
   // Set the response HTTP header with HTTP status and Content type
-  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.writeHead(200, { "Content-Type": "text/plain" })
 
   // Send the response body "Hello World"
-  res.end("Hello World\n");
+  res.end("Hello World\n")
 });
 
 // Prints a log once the server starts listening
-server.listen(port, hostname, function () {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(config.listenPort, config.listenHostname, function () {
+  //console.log(`Server running at http://${hostname}:${port}/`)
 });
