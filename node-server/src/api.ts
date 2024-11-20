fa854e8c20dfc84921cbf79d7759a17674b12a56
Kiki <mullerkrisztianbusiness@gmail.com>
2024-11-20 06:08:38 +0100

    API absztrakt cuccai.

--- node-server/src/api.ts
+++ node-server/src/api.ts
@@ -1,13 +1,29 @@
 import http from 'http'
+import { User, Offer, Category, Record } from './records.js'
 
 
-export class Api {
+export abstract class Api {
 
   constructor() {
     // do nothing
   }
 
   async handle(req: http.IncomingMessage, res: http.ServerResponse<http.IncomingMessage>): Promise<void> {
+    // TODO
   }
+
+  abstract fetchUser(id: number): User | undefined
+  abstract commitUser(val: User): void
+  abstract dropUser(val: User): void
+
+  abstract fetchOffer(id: number): Offer | undefined
+  abstract commitOffer(val: Offer): void
+  abstract dropOffer(val: Offer): void
+
+  abstract fetchCategory(id: number): Category | undefined
+  abstract commitCategory(val: Category): void
+  abstract dropCategory(val: Category): void
+
+
 
 }
